553789a72d6786dd97647ac216a56d6170239d11
Samirul Haque <samirulhaque08@gmail.com>
2022-02-27 17:04:11 +0530

    Update settings.py

--- PhoneSearch/PhoneSearch/settings.py
+++ PhoneSearch/PhoneSearch/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!_@e=84apjg@o+5ya!e&cqbzka03tk*p%8#7o5q&!ebhh$@xt'
+SECRET_KEY = 'PUT OWN KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

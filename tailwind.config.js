eaadf9bd966165737932f11dc3104ece6f456d0e
Samirul Haque <samirulhaque08@gmail.com>
2022-02-27 19:18:54 +0900

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: 'jit',
+  darkMode: 'class',
+  content: ['./PhoneSearch/base/templates/base/*.html'],
+  theme: {
+    extend: {
+      spacing:{
+        '100':'25rem',
+        '120': '30rem',
+        '132': '35rem',
+        '135': '40rem',
+       }
+    },
+  },
+  plugins: [],
+  
+}
